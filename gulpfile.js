92ef991357a87fc09603539a0b060547226081c2
Maksym Drobot <drobotm.com@gmail.com>
2022-06-24 01:13:40 +0300

    готова збірка Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
-const gulp = require("gulp");
-// const sassCommon = require('sass');
-const sass = require("gulp-sass")(require("sass"));
-const del = require("del");
-const rename = require("gulp-rename");
-const cleanCSS = require("gulp-clean-css");
-const babel = require("gulp-babel");
-const uglify = require("gulp-uglify");
-const concat = require("gulp-concat");
+import gulp from "gulp";
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import del from "del";
+import rename from "gulp-rename";
+import cleanCSS from "gulp-clean-css";
+import babel from "gulp-babel";
+import uglify from "gulp-uglify"; // зменшує файл
+import concat from "gulp-concat";
+import sourcemaps from 'gulp-sourcemaps'; // можна змінити шлях до стилів чи джаваскріпта. index.html буде підтягувати файли з якими ми працюємо, а не зкомпільовані - ті що на виході   (від dist до src)
+import autoprefixer from 'gulp-autoprefixer';
+import imagemin from 'gulp-imagemin';
+import fileinclude from "gulp-file-include";
+
+const sass = gulpSass(dartSass);
 
 const paths = {
+  html: {
+    src: "./*.html",
+    dest: "dist/"
+  },
   styles: {
     src: "src/styles/**/*.scss",
     dest: "dist/css/",
@@ -17,31 +27,60 @@ const paths = {
     src: "src/scripts/**/*.js",
     dest: "dist/js/",
   },
+  images: {
+    src: 'src/img/*',
+    dest: 'dist/img'
+  }
 };
 
+function html() {
+  return gulp.src(paths.html.src)
+    .pipe(fileinclude())
+    .pipe(gulp.dest(paths.html.dest))
+}
+
 function styles() {
-  return gulp
-    .src(paths.styles.src)
+  return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false,
+      grid: true
+		}))
     .pipe(cleanCSS())
     .pipe(
       rename({
         basename: "main",
         suffix: ".min",
       })
-    )
+  )
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.styles.dest));
 }
 
 function scripts() {
-  return gulp
-    .src(paths.scripts.src, {
+  return gulp.src(paths.scripts.src, {
       sourcemaps: true,
-    })
-    .pipe(babel())
+  })
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
     .pipe(uglify())
     .pipe(concat("main.min.js"))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.scripts.dest))
+}
+
+function img() {
+  return gulp.src(paths.images.src)
+		.pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            interlaced: true,
+            optimization: 5
+        }))
+		.pipe(gulp.dest(paths.images.dest))
 }
 
 function clean() {
@@ -49,15 +88,12 @@ function clean() {
 }
 
 function watch() {
+  gulp.watch(paths.html.src, html);
   gulp.watch(paths.styles.src, styles);
   gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.images.src, img);
 }
 
-const build = gulp.series(clean, gulp.parallel(styles, scripts), watch);
+const dev = gulp.series(clean, gulp.parallel(html, styles, scripts, img), watch);
 
-exports.clean = clean;
-exports.styles = styles;
-exports.scripts = scripts;
-exports.watch = watch;
-exports.build = build;
-exports.default = build;
+gulp.task('default', dev);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const capitalizeName = (str) => {
   const personNameArray = str.split(' ');
   let resultArray = [];
